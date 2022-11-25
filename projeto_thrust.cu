714104972fa375738dca22669a0b1c8c1d2409df
TheoBR00 <theobr17@gmail.com>
2022-11-25 17:25:02 -0300

    Corrigindo erro da conta 10

--- projeto_thrust.cu
+++ projeto_thrust.cu
@@ -302,10 +302,8 @@ int main(){
 
                 thrust::counting_iterator<int> iterador(0);
 
-                thrust::transform(iterador, iterador+(n-1), array_float.begin(), swap_vec(thrust::raw_pointer_cast<graph>(array.data()), n));
-                for(int it = 0; it < n; it++){
-                  std::cout << "swap vec_pontos: " << static_cast<graph>(vec_pontos[it]).id << std::endl;
-                }
+                thrust::transform(iterador, iterador+(n-1), array_float.begin(), swap_vec(thrust::raw_pointer_cast(array.data()), n));
+                
             }
             
                 
